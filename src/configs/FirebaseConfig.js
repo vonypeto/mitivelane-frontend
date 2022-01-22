58dd957e72915379242bfcb24573e6d7caf9ccb3
vonypeto <von.aralar@gmail.com>
2022-01-22 20:18:42 +0800

    Added Production

--- src/configs/FirebaseConfig.js
+++ src/configs/FirebaseConfig.js
@@ -1,3 +1,4 @@
+//currently running development
 const FirebaseConfig = {
   apiKey: "AIzaSyBvQkJqwD7QoVOqqV8lqLiUKYXvbYFfGJo",
   authDomain: "barangay-dev.firebaseapp.com",
@@ -11,6 +12,21 @@ const FirebaseConfig = {
 
 export default FirebaseConfig;
 
+//production
+// export default FirebaseConfig;
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCAAc0g25FpI-SP_EA_UJfitoa7srldN68",
+//   authDomain: "barangay-production.firebaseapp.com",
+//   databaseURL: "https://barangay-production-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "barangay-production",
+//   storageBucket: "barangay-production.appspot.com",
+//   messagingSenderId: "411790228525",
+//   appId: "1:411790228525:web:de1b2c97e032e2920b0ca4",
+//   measurementId: "G-0FY64HXB2X"
+// };
+
+//development
+
 // const FirebaseConfig = {
 //   apiKey: "AIzaSyBvQkJqwD7QoVOqqV8lqLiUKYXvbYFfGJo",
 //   authDomain: "barangay-dev.firebaseapp.com",
@@ -21,5 +37,3 @@ export default FirebaseConfig;
 //   messagingSenderId: "649867115560",
 //   appId: "1:649867115560:web:1477467cdadddab2eac91d",
 // };
-
-// export default FirebaseConfig;
