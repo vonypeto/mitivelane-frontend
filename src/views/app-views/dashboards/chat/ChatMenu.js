0dec6390b11b97e3c89367de15121cef026fa5e5
Rojhon <rojhonbuenaventura5@gmail.com>
2022-04-24 09:11:59 +0800

    basic chat

--- src/views/app-views/dashboards/chat/ChatMenu.js
+++ src/views/app-views/dashboards/chat/ChatMenu.js
@@ -1,29 +1,36 @@
-import React, { useState } from 'react'
-import ChatData from "assets/data/chat.data.json"
-import { Badge, Input } from 'antd';
+import React, { useState, useEffect } from 'react'
+import { Badge, Input, Avatar } from 'antd';
+import utils from "utils";
 import AvatarStatus from 'components/shared-components/AvatarStatus';
 import { COLOR_1 } from 'constants/ChartConstant';
 import { SearchOutlined } from '@ant-design/icons';
 import { useHistory } from "react-router-dom";
 
-const ChatMenu = ({ match, location }) => {
-	const [list, setList] = useState(ChatData);
+const ChatMenu = ({ match, location, chatData }) => {
+	const [list, setList] = useState([]);
+
+	useEffect(() => {
+		setList(chatData)
+
+	}, [chatData])
+
 	let history = useHistory();
-	const openChat = id => {
-		const data = list.map( elm => {
-			if(elm.id === id) {
+
+	const openChat = _id => {
+		const data = list.map(elm => {
+			if (elm._id === _id) {
 				elm.unread = 0
 			}
 			return elm
 		})
 		setList(data)
-		history.push(`${match.url}/${id}`)
+		history.push(`${match.url}/${_id}`)
 	}
 
 	const searchOnChange = e => {
 		const query = e.target.value;
-		const data = ChatData.filter(item => {
-			return query === ''? item : item.name.toLowerCase().includes(query)
+		const data = chatData.filter(item => {
+			return query === '' ? item : item.name.toLowerCase().includes(query)
 		})
 		setList(data)
 	}
@@ -33,26 +40,54 @@ const ChatMenu = ({ match, location }) => {
 	return (
 		<div className="chat-menu">
 			<div className="chat-menu-toolbar">
-				<Input 
-					placeholder="Search" 
+				<Input
+					placeholder="Search"
 					onChange={searchOnChange}
 					prefix={
-						<SearchOutlined className="font-size-lg mr-2"/>
+						<SearchOutlined className="font-size-lg mr-2" />
 					}
 				/>
 			</div>
 			<div className="chat-menu-list">
 				{
-					list.map( (item, i) => (
-						<div 
-							key={`chat-item-${item.id}`} 
-							onClick={() => openChat(item.id)} 
-							className={`chat-menu-list-item ${i === (list.length - 1)? 'last' : ''} ${item.id === id? 'selected' : ''}`}
+					list.map((item, i) => (
+						<div
+							key={`chat-item-${item._id}`}
+							onClick={() => openChat(item._id)}
+							className={`chat-menu-list-item ${i === (list.length - 1) ? 'last' : ''} ${item.id === id ? 'selected' : ''}`}
 						>
-							<AvatarStatus src={item.avatar} name={item.name} subTitle={item.msg[item.msg.length - 1].text}/>
+
+
+							<div className="d-flex align-items-center">
+								{
+									item.avatar ?
+										<>
+											<AvatarStatus src={item.avatar} name={item.name} subTitle={item.messages[item.messages.length - 1].content} />
+										</>
+										:
+										<>
+											<Avatar
+												size={40}
+												className="font-size-sm"
+												style={{ backgroundColor: "#111111" }}
+											>
+												{utils.getNameInitial(item.name)}
+											</Avatar>
+
+											<div className="ml-2">
+												<div className="avatar-status-name">{item.name}</div>
+												<div className="text-muted avatar-status-subtitle">
+													{(item.messages[item.messages.length - 1].content.length < 20) ? `${item.messages[item.messages.length - 1].content}`
+														: `${item.messages[item.messages.length - 1].content.substring(0, 20)}...`}
+												</div>
+											</div>
+										</>
+								}
+							</div>
+
 							<div className="text-right">
 								<div className="chat-menu-list-item-time">{item.time}</div>
-								{item.unread === 0 ? <span></span> : <Badge count={item.unread} style={{backgroundColor: COLOR_1}}/>}
+								{item.unread === 0 ? <span></span> : <Badge count={item.unread} style={{ backgroundColor: COLOR_1 }} />}
 							</div>
 						</div>
 					))
--- src/views/app-views/dashboards/chat/index.js
+++ src/views/app-views/dashboards/chat/index.js
@@ -1,15 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { message } from "antd"
+import ChatData from "assets/data/chat.data.json"
 import InnerAppLayout from 'layouts/inner-app-layout';
 import ChatContent from './ChatContent';
 import ChatMenu from './ChatMenu';
 import { withRouter } from "react-router-dom";
+import { AUTH_TOKEN } from "redux/constants/Auth";
+import axios from "axios";
+import { useAuth } from "contexts/AuthContext";
+
+var conversationData = []
+var alreadyRun = false
 
 const Chat = props => {
+	const { currentBarangay, generateToken } = useAuth();
+	const authToken = localStorage.getItem(AUTH_TOKEN);
+	const [chatData, setChatData] = useState(conversationData)
+
+	useEffect(() => {
+		if(alreadyRun == false){
+		  getConversations()
+		  alreadyRun = true
+		}
+
+	}, [])
+	
+	useEffect(() => {
+		conversationData = chatData
+	}, [chatData])
+
+	const getConversations = () => {
+		// setChatData(ChatData)
+
+		axios
+			.get("/api/chat/get-conversation/" + authToken, generateToken()[1])
+			.then((response) => {
+				console.log("Conversation in Index.js")
+				setChatData(response.data)
+			})
+			.catch(() => {
+				message.error("Could not fetch the data in the server!");
+			});
+	}
+
 	return (
 		<div className="chat">
-			<InnerAppLayout 
-				sideContent={<ChatMenu {...props}/>}
-				mainContent={<ChatContent {...props}/>}
+			<InnerAppLayout
+				sideContent={<ChatMenu match={props.match} location={props.location} chatData={chatData} />}
+				mainContent={<ChatContent {...props} chatData={chatData} setChatData={setChatData} />}
 				sideContentWidth={450}
 				sideContentGutter={false}
 				border
