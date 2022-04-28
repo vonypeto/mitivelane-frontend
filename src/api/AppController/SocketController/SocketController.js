67209adc5730941e49fece0495ff917912925351
vonypeto <von.aralar@gmail.com>
2022-04-28 12:00:33 +0800

    revert

--- src/api/AppController/SocketController/SocketController.js
+++ src/api/AppController/SocketController/SocketController.js
@@ -2,14 +2,15 @@ import { io } from "socket.io-client";
 import { AUTH_TOKEN } from "redux/constants/Auth";
 const IS_PROD = process.env.NODE_ENV === "production";
 const URL = IS_PROD
-  ? `https://${window.location.hostname}`
+  ? "https://mitivelane.herokuapp.com/"
   : "http://localhost:3000";
 export const socket = io(URL, {
+  withCredentials: true,
   credentials: true,
+  methods: ["GET", "POST"],
 });
 
 const authToken = localStorage.getItem(AUTH_TOKEN);
-console.log(window.location.hostname);
 console.log(process.env.NODE_ENV);
 socket.on("connect", () => {
   console.log("Im Connected to Socket id ", socket.id);
