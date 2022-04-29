8fa8f12d7f0ed4d72c2a6baec5fc742a648102ce
Rojhon <rojhonbuenaventura5@gmail.com>
2022-04-29 14:19:50 +0800

    wss

--- src/api/AppController/SocketController/SocketController.js
+++ src/api/AppController/SocketController/SocketController.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 import { AUTH_TOKEN } from "redux/constants/Auth";
 const IS_PROD = process.env.NODE_ENV === "production";
 const URL = IS_PROD
-  ? "ws://mitivelane.herokuapp.com/"
+  ? "wss://mitivelane.herokuapp.com/"
   : "ws://localhost:3000";
 export const socket = io(URL, {
   withCredentials: true,
