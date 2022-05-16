2d0aeb5b9dd2d70f13c865ab96ba2a8698ccef80
Rojhon <rojhonbuenaventura5@gmail.com>
2022-05-16 09:31:47 +0800

    Chat

--- src/views/app-views/dashboards/chat/index.js
+++ src/views/app-views/dashboards/chat/index.js
@@ -1,101 +1,116 @@
 import React, { useState, useEffect } from "react";
-import { message } from "antd";
-import ChatData from "assets/data/chat.data.json";
-import InnerAppLayout from "layouts/inner-app-layout";
-import ChatContent from "./ChatContent";
-import ChatMenu from "./ChatMenu";
+import { message } from "antd"
+import ChatData from "assets/data/chat.data.json"
+import InnerAppLayout from 'layouts/inner-app-layout';
+import ChatContent from './ChatContent';
+import ChatMenu from './ChatMenu';
 import { withRouter } from "react-router-dom";
 import { AUTH_TOKEN } from "redux/constants/Auth";
 import axios from "axios";
-
 import { useAuth } from "contexts/AuthContext";
-import { SocketContext, socket } from "contexts/SocketContext";
-import { ChatContext } from "contexts/ChatContext";
+import { socket } from "api/AppController/SocketController/SocketController"
 
-var conversationData = [];
-var alreadyRun = false;
+var conversationData = []
+var alreadyRun = false
 
-const Chat = (props) => {
-  const { currentOrganization, generateToken } = useAuth();
-  const authToken = localStorage.getItem(AUTH_TOKEN);
-  const [chatData, setChatData] = useState(conversationData);
-  useEffect(() => {
-    if (alreadyRun == false) {
-      getConversations();
-      alreadyRun = true;
-      console.log("Socket IO Connection in Context API");
-    }
-  }, []);
-  useEffect(() => {
-    conversationData = chatData;
-  }, [chatData]);
+const Chat = props => {
+	const { currentBarangay, generateToken } = useAuth();
+	const authToken = localStorage.getItem(AUTH_TOKEN);
+	const [chatData, setChatData] = useState(conversationData)
 
-  useEffect(() => {
-    socket
-      .off("chat:receive-message")
-      .on("chat:receive-message", (conversationId, message) => {
-        message.from = "opposite";
-        console.log(message);
+	useEffect(() => {
+		if (alreadyRun == false) {
+			getConversations()
+			alreadyRun = true
+			console.log("Test 1 Chat muna sa console")
+		}
 
-        const newData = conversationData.filter(
-          (elm) => elm._id === conversationId
-        );
+	}, [])
 
-        newData[0].messages.push(message);
+	useEffect(() => {
+		conversationData = chatData
+	}, [chatData])
 
-        var currentData = conversationData.filter(
-          (elm) => elm._id !== conversationId
-        );
+	useEffect(() => {
+		socket.off("chat:receive-message").on("chat:receive-message", (conversationId, message) => {
+			message.from = "opposite"
+			console.log(message)
+			
+			const newData = conversationData.filter(elm => elm._id === conversationId)
+			
+			newData[0].messages.push(message)
+		
 
-        setChatData(finalValue);
-      });
-  }, [socket]);
+			var currentData = conversationData.filter((elm) => elm._id !== conversationId)
 
-  const startConversation = (values) => {
-    axios
-      .post("/api/chat/start-conversation", values, generateToken()[1])
-      .then((response) => {
-        if (response.data == "Success") {
-          return message.success(`Added new aralotter`);
-        } else {
-          return message.error("Error, please try again.");
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        message.destroy();
-        message.error("The action can't be completed, please try again.");
-      });
-  };
+			var finalValue = newData.concat(currentData)
 
-  const getConversations = () => {
-    // setChatData(ChatData)
+			setChatData(finalValue)
+			
+			{
+				/*
+			const newData = conversationData.filter(elm => elm._id === conversationId)
+			
+			newData[0].messages.push(message)
+		
 
-    axios
-      .get("/api/chat/get-conversation/" + authToken, generateToken()[1])
-      .then((response) => {
-        console.log("Conversation in Index.js ", response.data);
-        setChatData(response.data);
-      })
-      .catch(() => {
-        message.error("Could not fetch the data in the server!");
-      });
-  };
-  return (
-    <div className="chat">
-      <SocketContext.Provider value={socket}>
-        <ChatContext.Provider value={{ chatData, setChatData }}>
-          <InnerAppLayout
-            sideContent={<ChatMenu {...props} />}
-            mainContent={<ChatContent {...props} />}
-            sideContentWidth={450}
-            sideContentGutter={false}
-            border
-          />
-        </ChatContext.Provider>
-      </SocketContext.Provider>
-    </div>
-  );
-};
+			var currentData = conversationData.filter((elm) => elm._id !== conversationId)
 
-export default withRouter(Chat);
+			var finalValue = newData.concat(currentData)
+
+			setChatData(finalValue)
+				*/
+			}
+
+		})
+
+	}, [socket])
+
+	const startConversation = (values) => {
+		axios
+			.post("/api/chat/start-conversation", values, generateToken()[1])
+			.then((response) => {
+				if (response.data == "Success") {
+					return message.success(
+						`Added new Blotter`
+					);
+				} else {
+					return message.error("Error, please try again.");
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+				message.destroy();
+				message.error("The action can't be completed, please try again.");
+			});
+
+	}
+
+	const getConversations = () => {
+		// setChatData(ChatData)
+
+		axios
+			.get("/api/chat/get-conversation/" + authToken, generateToken()[1])
+			.then((response) => {
+				console.log("Conversation in Index.js ", response.data)
+				setChatData(response.data)
+			})
+			.catch(() => {
+				message.error("Could not fetch the data in the server!");
+			});
+	}
+
+	return (
+		<div className="chat">
+			<InnerAppLayout
+				sideContent={<ChatMenu match={props.match} location={props.location} chatData={chatData} />}
+				mainContent={<ChatContent {...props} chatData={chatData} setChatData={setChatData} socket={socket} />}
+				sideContentWidth={450}
+				sideContentGutter={false}
+				border
+			/>
+		</div>
+	)
+}
+
+export default withRouter(Chat)
